119c77aed8df459cb424faa8d7a8d1d84d8f7ef0
Levik Naparstek <levik010@gmail.com>
2019-01-31 20:17:04 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -12,4 +12,7 @@
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
 def speak_to_grandma
-  if "Hi Granny."
+  if "Hi Granny.upcase"
+    puts "NO, NOT SINCE 1938!"
+    
+  elseif "I love you GRANDMA"
